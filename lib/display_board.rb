87bf4d07a291f0ff073dfe9e48c2662707c37aa3
Radele Rose <radele.rose@gmail.com>
2020-08-16 07:04:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 board = [" "," "," "," "," "," "," "," "," "]
-display_board(board)
+
+def display_board(board)
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
+
+end
